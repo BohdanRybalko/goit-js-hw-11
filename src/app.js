ba8b28d6c9da48eba00d4243af31c84cb7e63763
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2023-12-14 10:12:06 +0200

    fix problem

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import notiflix from 'notiflix';
+import { renderImages } from './gallery';
 
 const API_KEY = '41213110-775d8605f206f9e1ea3c7d6a2';
 const form = document.getElementById('search-form');
-const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 let page = 1;
 
@@ -77,22 +77,4 @@ function handleImageData(data) {
   } else {
     loadMoreBtn.style.display = 'block';
   }
-}
-
-function renderImages(images) {
-  const cardsHTML = images.map(
-    image => `
-    <div class="photo-card">
-      <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-      <div class="info">
-        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-        <p class="info-item"><b>Views:</b> ${image.views}</p>
-        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
-      </div>
-    </div>
-  `
-  );
-
-  gallery.innerHTML = cardsHTML.join('');
 }
---
+++ src/gallery.js
@@ -0,0 +1,21 @@
+function renderImages(images) {
+  const gallery = document.querySelector('.gallery');
+
+  const cardsHTML = images.map(
+    image => `
+    <div class="photo-card">
+      <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+        <p class="info-item"><b>Views:</b> ${image.views}</p>
+        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+      </div>
+    </div>
+  `
+  );
+
+  gallery.innerHTML = cardsHTML.join('');
+}
+
+export { renderImages };
